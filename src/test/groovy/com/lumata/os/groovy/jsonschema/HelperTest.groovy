9daef09610f52d24d067cdfe14fc6f0daed24441
Alexander De Leon <me@alexdeleon.name>
2012-04-10 11:01:05 +0200

    Initialized project

---
+++ src/test/groovy/com/lumata/os/groovy/jsonschema/HelperTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.lumata.os.groovy.jsonschema
+
+import groovy.util.GroovyTestCase
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
